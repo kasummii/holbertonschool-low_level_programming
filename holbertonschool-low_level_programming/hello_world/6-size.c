33a45662bff70d368b32bf57a8517bbfaae74caf
kasummii <kasumi20110211@gmail.com>
2025-06-09 19:06:38 +0000

    6. Size of various types

--- holbertonschool-low_level_programming/hello_world/6-size.c
+++ holbertonschool-low_level_programming/hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
+ * main - prints sizes of various types on the computer it runs on
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,6 +12,5 @@ int main(void)
     printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
     printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
     printf("Size of a float: %lu byte(s)\n", sizeof(float));
-
     return (0);
 }
