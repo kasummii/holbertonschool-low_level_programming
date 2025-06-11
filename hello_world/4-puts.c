e3b414a3fcd0212b1f8a56efba63279d12d8c123
kasummii <kasumi20110211@gmail.com>
2025-06-11 13:43:03 +0000

    Add 4-puts.c printing message using puts function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
+<<<<<<< HEAD
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle\"");
+=======
+/**
+ * main - prints a message using puts
+ * Return: 0 if success
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+>>>>>>> 7bd6f15 (Add 4-puts.c printing message using puts function)
     return (0);
 }
