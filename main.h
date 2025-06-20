da4660ecd0798d9c3c30c5589ba76f5022fbbcad
kasummii <kasumi20110211>
2025-06-20 12:40:22 +0000

    hello

--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
-void more_numbers(void); /* <- required for this task */
+void more_numbers(void);
+void print_line(int n);
 
 #endif
