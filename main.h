18c425894f1d60c5e3f2b03a520e904a3039c27b
kasummii <kasumi20110211>
2025-06-20 12:35:34 +0000

    hello

--- main.h
+++ main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
+void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void); /* <- required for this task */
 
-#endif /* MAIN_H */
+#endif
