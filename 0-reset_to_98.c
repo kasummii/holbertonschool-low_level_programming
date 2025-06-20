7f39e8b82be23c34f57570f36cc2b1fb89bff978
kasummii <kasumi20110211>
2025-06-20 13:32:13 +0000

    hello

--- 0-reset_to_98.c
+++ 0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value pointed to by n to 98
- * @n: pointer to an int
+ * reset_to_98 - Updates the value pointed to by a pointer to 98
+ * @n: Pointer to an int
  */
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-my readme
+# C - Pointers, arrays and strings
+
+This project focuses on understanding pointers, arrays, and how to manipulate memory in C.
